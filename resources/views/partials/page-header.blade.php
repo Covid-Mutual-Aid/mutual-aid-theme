c6ecf2301b701e948f9048256e4b460614b35f5d
Rob Morrissey <robmorrissey@gmail.com>
2020-05-01 15:52:10 +0100

    Optimised SVG using OMG SVG. Some font styling.

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,4 +1,4 @@
 <div class="page-header">
-  <h1>{!! App::title() !!}</h1>
-  <p>{!! App::tagline() !!}</p>
+  <h1 class="text-5xl font-normal">{!! App::title() !!}</h1>
+  <p class="text-2xl">{!! App::tagline() !!}</p>
 </div>
