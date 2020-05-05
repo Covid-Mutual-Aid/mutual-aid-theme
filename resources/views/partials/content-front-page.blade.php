c51708d55d9e87b290bf55e2aa928278c577cf31
Rob Morrissey <robmorrissey@gmail.com>
2020-05-06 00:40:55 +0100

    Implemented basic content styles

--- resources/views/partials/content-front-page.blade.php
+++ resources/views/partials/content-front-page.blade.php
@@ -1,2 +1,2 @@
-<div class="container"> @php the_content() @endphp </div>
+<div class="container -mt-20"> @php the_content() @endphp </div>
 {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
